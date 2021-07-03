6f86b1a935bc31a3d55d47889e86b17c6acc52ab
Aleksa Marković <28643848+topofkeks@users.noreply.github.com>
2021-07-04 00:13:01 +0200

    Mora dupli prolaz zbog TOC

--- combine.sh
+++ combine.sh
@@ -16,7 +16,9 @@ if [ "$1" == "print" ]
 then
     pandoc combined-print.md -s -t latex -o combined-print.tex
     pdflatex --enable-installer --shell-escape combined-print.tex
+    pdflatex --enable-installer --shell-escape combined-print.tex
 else
     pandoc combined-web.md -s -t latex -o combined-web.tex
+    pdflatex --enable-installer --shell-escape combined-web.tex 
     pdflatex --enable-installer --shell-escape combined-web.tex 
 fi
